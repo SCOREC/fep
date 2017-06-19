90552545d2fc6e352eb5005974ba0d817bdc6746
Cameron Smith <cwsmith@users.noreply.github.com>
2017-06-19 16:18:57 -0400

    full path to mpicxx

--- a1/setup.sh
+++ a1/setup.sh
@@ -4,6 +4,6 @@ set -x
 [ ! -e build ] && mkdir build
 cd build
 flags='-g -O2'
-cmake -DCMAKE_CXX_COMPILER=mpicxx -DCMAKE_CXX_FLAGS=$flags ../
+cmake -DCMAKE_CXX_COMPILER=`which mpicxx` -DCMAKE_CXX_FLAGS=$flags ../
 cd -
 set +x
