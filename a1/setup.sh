588d04c731b3f278814fdf1a04d33b5718266d43
Cameron Smith <smithc11@rpi.edu>
2019-01-24 10:30:53 -0500

    quote flags

--- a1/setup.sh
+++ a1/setup.sh
@@ -3,7 +3,7 @@ module load cmake xl proprietary/simmetrix/simModSuite proprietary/core-sim/xl
 set -x
 [ ! -e build ] && mkdir build
 cd build
-flags='-g -O2'
-cmake -DCMAKE_CXX_COMPILER=`which mpicxx` -DCMAKE_CXX_FLAGS=$flags ../
+flags="-g -O2"
+cmake -DCMAKE_CXX_COMPILER=`which mpicxx` -DCMAKE_CXX_FLAGS="$flags" ../
 cd -
 set +x
