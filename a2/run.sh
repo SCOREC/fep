3b009dd9d195848a4e6ebf0c9fd9a792b5b97ec0
Cameron Smith <smithc11@rpi.edu>
2021-01-29 22:18:10 -0500

    use vDev7a99c49_gcc910_1 install

--- a2/run.sh
+++ a2/run.sh
@@ -6,7 +6,7 @@
 
 gcc9libs=/gpfs/u/software/erp-rhel7/modulefiles/.gcc/9.1.0/1/lib64
 export LD_LIBRARY_PATH=$gcc9libs:$LD_LIBRARY_PATH
-module use /gpfs/u/software/erp-spack-install/v0160_gcc910_1/lmod/linux-centos7-x86_64/gcc/9.1.0/
+module use /gpfs/u/software/erp-spack-install/vDev7a99c49_gcc910_1/lmod/linux-centos7-x86_64/gcc/9.1.0/
 module load openmpi/4.0.1
 module load pumi/master-int32-sim
 
