7f5dfbe6ec125dd9f9834abcebcad537b9ac2595
Cameron Smith <smithc11@rpi.edu>
2023-01-09 16:39:21 -0500

    swap a2 and a3

--- a3/run.sh
+++ a3/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
-#SBATCH --job-name=fepsA3
+#SBATCH --job-name=fepsA2
 #SBATCH --partition=debug
 #SBATCH -t 00:10:00
-#SBATCH -n 2
+#SBATCH -n 1
 
 gcc9libs=/gpfs/u/software/erp-rhel7/modulefiles/.gcc/9.1.0/1/lib64
 export LD_LIBRARY_PATH=$gcc9libs:$LD_LIBRARY_PATH
-module use /gpfs/u/software/erp-spack-install/vDev7a99c49_gcc910_1/lmod/linux-centos7-x86_64/gcc/9.1.0/
-module load openmpi/4.0.1
+module use /gpfs/u/software/erp-spack-install/v0190_0/lmod/linux-centos7-x86_64/gcc/9.1.0
+module unuse /gpfs/u/software/erp-spack-install/lmod/linux-centos7-x86_64/Core
+module load gcc/9.1.0/1
+module load openmpi/3.1.6
 module load pumi/master-int32-sim
 
 exe=$1; shift
